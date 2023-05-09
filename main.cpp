20a3ccb7d2718dd382edb4b7c45f5ac6c43751f5
Sunandan Phalke <77063671+SunandanP@users.noreply.github.com>
2023-05-10 04:32:49 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,64 +10,56 @@ struct point
     GLfloat x, y, z;
 };
 
-int factorial(int n)
-{
-    if (n<=1)
-        return(1);
-    else
-        n=n*factorial(n-1);
-    return n;
-}
+void display();
+void bezierPlot(point*, int, int);
+int factorial(int);
+void BezierTakeControl(float, point*, int , point*, int*);
+void nCr(int,int*);
 
-void computeNcR(int n, int *hold_ncr_values)
+void BezierTakeControl(float t,
+                       point *bezierPoints,
+                       int numberOfControlPoints,
+                       point *controlPoints,
+                       int *ncrValues)
 {
-    int r;
-    for(r=0; r<=n; r++) //start from nC0, then nC1, nC2, nC3 till nCn
+    int i, n = numberOfControlPoints - 1;
+
+    float breinsteinPolynomial;
+
+    bezierPoints -> x = 0;
+    bezierPoints -> y = 0;
+    bezierPoints -> z = 0;
+
+    for(i=0; i < numberOfControlPoints; i++)
     {
-        hold_ncr_values[r] = factorial(n) / (factorial(n-r) * factorial(r));
+        breinsteinPolynomial = ncrValues[i] * pow(t, i) * pow(1 - t, n - i);
+
+        bezierPoints -> x += breinsteinPolynomial * controlPoints[i].x;
+        bezierPoints -> y += breinsteinPolynomial * controlPoints[i].y;
+        bezierPoints -> z += breinsteinPolynomial * controlPoints[i].z;
     }
 }
 
-void computeBezierPoints(float t, point *actual_bezier_point, int number_of_control_points, point *control_points_array, int *hold_ncr_values)
+void bezierPlot(point *controlPoints, int numberOfControlPoints, int numberOfBezierPoints)
 {
-    int i, n = number_of_control_points-1;
-
-    float bernstein_polynomial;
-
-    actual_bezier_point -> x = 0;
-    actual_bezier_point -> y = 0;
-    actual_bezier_point -> z = 0;
-
-    for(i=0; i < number_of_control_points; i++)
-    {
-        bernstein_polynomial = hold_ncr_values[i] * pow(t, i) * pow( 1-t, n-i);
-
-        actual_bezier_point -> x += bernstein_polynomial * control_points_array[i].x;
-        actual_bezier_point -> y += bernstein_polynomial * control_points_array[i].y;
-        actual_bezier_point -> z += bernstein_polynomial * control_points_array[i].z;
-    }
-}
-
-void bezier(point *control_points_array, int number_of_control_points, int number_of_bezier_points)
-{
-    point actual_bezier_point;
+    point bezierPoint;
     float t;
-    int *hold_ncr_values, i;
+    int *ncrValues, i;
 
-    hold_ncr_values = new int[number_of_control_points]; // to hold the nCr values
+    ncrValues = new int[numberOfControlPoints]; // to hold the nCr values
 
-    computeNcR(number_of_control_points-1, hold_ncr_values); // call nCr function to calculate nCr values
+    nCr(numberOfControlPoints - 1, ncrValues); // call nCr function to calculate nCr values
 
     glBegin(GL_LINE_STRIP);
-        for(i=0; i<=number_of_bezier_points; i++)
+        for(i=0; i <= numberOfBezierPoints; i++)
         {
-            t=float(i)/float(number_of_bezier_points);
-            computeBezierPoints(t, &actual_bezier_point, number_of_control_points, control_points_array, hold_ncr_values);
-            glVertex2f(actual_bezier_point.x, actual_bezier_point.y);
+            t=float(i)/float(numberOfBezierPoints);
+            BezierTakeControl(t, &bezierPoint, numberOfControlPoints, controlPoints, ncrValues);
+            glVertex2f(bezierPoint.x, bezierPoint.y);
         }
     glEnd();
 
-    delete[] hold_ncr_values;
+    delete[] ncrValues;
 }
 
 void display()
@@ -96,21 +88,21 @@ void display()
     for(int i=0; i<50; i++)
     {
         glTranslatef(0, -0.8, 0 );
-        bezier(control_points_array, number_of_control_points, number_of_bezier_points);
+        bezierPlot(control_points_array, number_of_control_points, number_of_bezier_points);
     }
 
     glColor3f(1, 1, 1); //Indian flag: white color code
     for(int i=0; i<50; i++)
     {
         glTranslatef(0, -0.8, 0);
-        bezier(control_points_array, number_of_control_points, number_of_bezier_points);
+        bezierPlot(control_points_array, number_of_control_points, number_of_bezier_points);
     }
 
     glColor3f(0, 1, 0); //Indian flag: green color code
     for(int i=0; i<50; i++)
     {
         glTranslatef(0, -0.8, 0);
-        bezier(control_points_array, number_of_control_points, number_of_bezier_points);
+        bezierPlot(control_points_array, number_of_control_points, number_of_bezier_points);
     }
 
     glPopMatrix();
@@ -134,13 +126,31 @@ void init()
     gluOrtho2D(0,500,0,500);
 }
 
+int factorial(int n)
+{
+    if (n<=1)
+        return(1);
+    else
+        n= n * factorial(n-1);
+    return n;
+}
+
+void nCr(int n, int *hold_ncr_values)
+{
+    int r;
+    for(r=0; r <= n; r++) //start from nC0, then nC1, nC2, nC3 till nCn
+    {
+        hold_ncr_values[r] = factorial(n) / (factorial(n-r) * factorial(r));
+    }
+}
+
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowPosition(0, 0);
     glutInitWindowSize(500,500);
-    glutCreateWindow("Bezier Curve - updated");
+    glutCreateWindow("Flag using Bazier Curve");
 
     init();
 
